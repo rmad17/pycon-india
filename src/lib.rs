756c9e89f40d727c21066b855519d94234c2cf50
Sourav Basu <souravbasu17@zohomail.in>
2024-04-02 07:58:13 +0530

    Adding csv with Box

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,22 @@
 use pyo3::prelude::*;
+use std::{error::Error, io, process};
 
 /// Formats the sum of two numbers as string.
+// fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+//     Ok((a + b).to_string())
+// }
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn sum_as_string() -> Result<(), Box<dyn Error>> {
+    // Build the CSV reader and iterate over each record.
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    for result in rdr.records() {
+        // The iterator yields Result<StringRecord, Error>, so we check the
+        // error here.
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
 }
 
 /// A Python module implemented in Rust. The name of this function must match
