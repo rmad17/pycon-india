04ff4d2fe19de4460dc879f0354043af8282d2d9
Sourav Basu <souravbasu17@zohomail.in>
2024-04-25 08:38:55 +0530

    PyO3 WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
-use pyo3::prelude::*;
-use std::{error::Error, io, process};
+use pyo3::prelude::*
+use std::{error::Error, io, process}
 
-/// Formats the sum of two numbers as string.
+// Formats the sum of two numbers as string.
 // fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 //     Ok((a + b).to_string())
 // }
 
 #[pyfunction]
-fn sum_as_string() -> Result<(), Box<dyn Error>> {
+fn sum_as_string() -> PyAny<T> {
     // Build the CSV reader and iterate over each record.
     let mut rdr = csv::Reader::from_reader(io::stdin());
     for result in rdr.records() {
         // The iterator yields Result<StringRecord, Error>, so we check the
         // error here.
-        let record = result?;
+        let record = result;
         println!("{:?}", record);
     }
-    Ok(())
+    Ok((rdr.records()))
 }
 
 /// A Python module implemented in Rust. The name of this function must match
