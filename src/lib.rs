9c84e3beee1d9b84aa0ae1c3381c16acc3b7f212
Sourav Basu <souravbasu17@gmail.com>
2024-12-09 00:52:26 +0530

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-use pyo3::prelude::PyErr;
 use pyo3::prelude::PyResult;
 use pyo3::prelude::*;
 use pyo3::pyfunction;
 use pyo3::pymodule;
-use pyo3::types::PyList;
 use pyo3::types::PyModule;
 use pyo3::wrap_pyfunction;
 use pyo3::Bound;
-
-use std::io;
+use std::error::Error;
 
 // Formats the sum of two numbers as string.
 // fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
@@ -16,19 +13,23 @@ use std::io;
 // }
 
 #[pyfunction]
-fn sum_as_string(filename: String) -> PyResult {
+fn csv_records(filename: String) -> PyResult<()> {
     // Build the CSV reader and iterate over each record.
-    let mut rdr = csv::Reader::from_reader(io::stdin());
+    let mut rdr = csv::Reader::from_path(filename)?;
     let mut records: Vec<String> = vec![String::new(); 126];
     for (index, result) in rdr.records().enumerate() {
-        // for result in rdr.records() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here.
         let record = result;
         println!("{:?}", record);
         records.insert(index, result);
     }
-    Ok(records);
+    Ok(records)
+    //for result in rdr.deserialize() {
+    //    // Notice that we need to provide a type hint for automatic
+    //    // deserialization.
+    //    let record = result?;
+    //    println!("{:?}", record);
+    //}
+    //Ok(())
 }
 
 /// A Python module implemented in Rust. The name of this function must match
@@ -36,6 +37,6 @@ fn sum_as_string(filename: String) -> PyResult {
 /// import the module.
 #[pymodule]
 fn pyrust_csv(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(csv_records, m)?)?;
     Ok(())
 }
